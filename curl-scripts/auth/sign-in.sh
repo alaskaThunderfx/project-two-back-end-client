5cc9c54757007c018e97f2f24e356dcca95c3d54
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2021-06-03 12:46:00 -0400

    User data interacts with db properly

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -9,7 +9,7 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "'"${EMAIL}"'",
+      "user_name": "'"${USERNAME}"'",
       "password": "'"${PASSWORD}"'"
     }
   }'
